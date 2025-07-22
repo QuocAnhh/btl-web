e2185b9830caed5fd160130727a661519a05ae61
QuocAnhh <anh16102004ok@gmail.com>
2025-07-22 21:37:38 +0700

    Cập nhật  cấu hình autoload, cache các file helper để đồng bộ với các thay đổi mới nhất trong ứng dụng

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -8,15 +8,15 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         api: __DIR__.'/../routes/api.php',
-        apiPrefix: 'api',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
+        apiPrefix: 'api',
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->alias([
             'admin' => \App\Http\Middleware\CheckIsAdmin::class,
         ]);
     })
-    ->withExceptions(function (Exceptions $exceptions): void {
+    ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
